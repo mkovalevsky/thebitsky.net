9f08684b6852037a45e5138a8de2a3fe460b4d81
maxkovalevski <me@kovalevski.net>
2022-05-18 00:00:06 +0200

    implemented storing count of subscribers on Revue

---
+++ scripts/getRevueSubscribersCount.js
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const { REVUE_API_KEY, REVUE_BASE_URL_API, REVUE_API_VERSION } = process.env;
+
+export const getRevueSubscribersCount = async () => {
+    try {
+        const response = await fetch(`${REVUE_BASE_URL_API}/${REVUE_API_VERSION}/subscribers`, {
+            headers: {
+                'Authorization': `Token ${REVUE_API_KEY}`,
+            },
+        });
+        const data = await response.json();
+
+        if (!data || !data.length) {
+            return 0;
+        }
+
+        return data.length;
+    } catch (e) {
+        console.error(`🔴 ${e}`);
+    }
+};
+
---
+++ scripts/storeRevueSubscribersCount.js
@@ -0,0 +1,41 @@
+import fetch from 'node-fetch';
+import * as dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { existsSync } from 'fs';
+import { writeFile, mkdir } from 'fs/promises';
+
+import { getRevueSubscribersCount } from './getRevueSubscribersCount.js';
+
+dotenv.config();
+
+const PROJECT_ROOT_PATH = process.cwd();
+
+const DATA_FOLDER_PATH = path.join(PROJECT_ROOT_PATH, '/data');
+const DATA_FILE_NAME = 'revueSubscribersCount.json';
+const DATA_FILE_PATH = path.join(DATA_FOLDER_PATH, DATA_FILE_NAME);
+
+const createDataFile = async (count) => {
+    const content = `{\n\t"count": ${count}\n}`;
+
+    try {
+        if (!existsSync(DATA_FOLDER_PATH)) {
+            await mkdir(DATA_FOLDER_PATH);
+        }
+
+        await writeFile(DATA_FILE_PATH, content, 'utf-8');
+        console.log('✅ File with data about count of subscribers was successfully created');
+    } catch(e) {
+        console.error(`🔴 ${e}`);
+    }
+}
+
+const storeRevueSubscribersCount = async () => {
+    const count = await getRevueSubscribersCount()
+    if (!count) {
+        return;
+    }
+    await createDataFile(count);
+};
+
+storeRevueSubscribersCount();
